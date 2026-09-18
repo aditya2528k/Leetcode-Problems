7596b6803ff5ab3018132c9534a0695c3d26b5b7
aditya2528k <adityaanand12004@gmail.com>
2026-09-18 13:30:14 +0530

    Time: 0 ms (100%), Space: 43.7 MB (43.78%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,27 +1,34 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
         List<String> res = new ArrayList<>();
-        if(digits == null || digits.length() == 0){
+        if (digits.length() == 0) {
             return res;
         }
-
-        String[] map = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        combo(res, new StringBuilder(), digits, 0, map);
+        HashMap<Character, String> map = new HashMap<>();
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
+        StringBuilder diary = new StringBuilder();
+        fun(digits, 0, map, res, diary);
         return res;
     }
 
-    private void combo(List<String> res, StringBuilder curr, String digits, int index, String[] map){
-         if (index == digits.length()) {
-            res.add(curr.toString());
+    void fun(String s, int index, HashMap<Character, String> map,
+        List<String> res, StringBuilder diary) {
+        if (index == s.length()) {
+            res.add(diary.toString());
             return;
         }
-
-        String letters = map[digits.charAt(index) - '0'];
-
-        for (char ch : letters.toCharArray()) {
-            curr.append(ch);
-            combo(res, curr, digits, index + 1, map);
-            curr.deleteCharAt(curr.length() - 1);
+        String choice = map.get(s.charAt(index));
+        for (int j = 0; j < choice.length(); j++) {
+            diary.append(choice.charAt(j));
+            fun(s, index + 1, map, res, diary);
+            diary.deleteCharAt(diary.length() - 1);
         }
     }
 }
